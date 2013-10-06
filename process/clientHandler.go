f9c4d015178349eac1a2e15fe30144e000a91ca4
Shayan Pooya <shayan@liveve.org>
2013-10-05 22:13:03 -0600

    Add a handler for the http commands.

--- process/clientHandler.go
+++ process/clientHandler.go
@@ -5,12 +5,31 @@ import (
 	"net/http"
 )
 
-func clientHandler(w http.ResponseWriter, r *http.Request) {
+func getStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Status is: %d\n", getMyState())
 }
 
+func getLeader(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "not me")
+}
+
+const lenPath = len("/command/")
+
+/*
+Command is a number that we should set the status to.
+    http://<>/command/id/number
+*/
+func processCommand(w http.ResponseWriter, r *http.Request) {
+	idAndNumber := r.URL.Path[lenPath:]
+	var id, number int
+	fmt.Sscanf(idAndNumber, "%d/%d", &id, &number)
+	fmt.Fprintf(w, "Command is: %d, id is: %d\n", id, number)
+}
+
 func startServer(port int) error {
-	http.HandleFunc("/", clientHandler)
+	http.HandleFunc("/status", getStatus)
+	http.HandleFunc("/leader", getLeader)
+	http.HandleFunc("/command/", processCommand)
 	strPort := fmt.Sprintf(":%d", port)
 	return http.ListenAndServe(strPort, nil)
 }
